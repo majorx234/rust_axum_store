2e2be0eb935067021b7a6758605731fade56863e
MajorX234 <majorx234@googlemail.com>
2026-01-24 22:55:41 +0100

    Add server config handler

---
+++ src/config.rs
@@ -0,0 +1,31 @@
+use std::net::SocketAddr;
+use std::str::FromStr;
+
+#[derive(Clone, Debug)]
+pub struct Config {
+    pub host_ip: String,
+    pub port: u32,
+    pub rust_log: String,
+ }
+
+impl Config {
+    /// Config Constructor, reads env variables and sets config
+    pub fn new() -> Config {
+        let host_ip = std::env::var("HOST_IP").expect("HOST_IP not set");
+        let port = std::env::var("PORT").map_or(8080, |port_string| {
+            port_string.parse::<u32>().expect("PORT not parsable")
+        });
+        let rust_log = std::env::var("RUST_LOG").unwrap_or_else(|_| "todo_axum=debug,tower_http=debug".into());
+       Config {
+            host_ip,
+           port,
+           rust_log,
+        }
+    }
+    pub fn get_host_socket_addr(&self) -> SocketAddr {
+        SocketAddr::from_str(&format!("{}:{}", self.host_ip, self.port)[..]).unwrap()
+    }
+    pub fn get_rust_log(&self) -> &str {
+        &self.rust_log
+    }
+}
