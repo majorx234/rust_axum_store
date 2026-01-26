298b54f9d68faecac72bef04082b8c5a557026a9
MajorX234 <majorx234@googlemail.com>
2026-01-27 00:30:24 +0100

    Add hasher trait to handle different hash functions

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,4 @@ pub mod router;
 pub mod server_state;
 pub mod error;
 pub mod store_core;
+pub mod store_app;
--- src/store_app/user_management.rs
+++ src/store_app/user_management.rs
@@ -3,18 +3,29 @@ use async_trait::async_trait;
 
 #[async_trait]
 pub trait UserDatabase: Send + Sync {
-    async fn create_user(&self, username: &str);
+    async fn create_user(&self, username: &str, password_hash: &str);
+}
+
+pub trait UserPasswordHasher: Send + Sync {
+    fn hash_password(&self, password: &str) -> &str;
 }
 
 #[derive(Clone)]
 pub struct UserPool{
-    user_database: Arc<dyn UserDatabase>
+    hasher: Arc<dyn UserPasswordHasher>,
+    user_database: Arc<dyn UserDatabase>,
 }
 
 impl UserPool{
-    pub fn new(user_database: Arc<dyn UserDatabase>) -> Self {
+    pub fn new(user_database: Arc<dyn UserDatabase>, hasher: Arc<dyn UserPasswordHasher>) -> Self {
         Self {
+            hasher,
             user_database
         }
+    }
+
+    pub async fn add(&self, username: &str, password: &str) {
+        let hash = &self.hasher.hash_password(password);
+        self.user_database.create_user(username, hash).await;
     }
 }
