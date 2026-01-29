c00e10311ae58cbf24ee0a9c03bb60b6e1d9b175
MajorX234 <majorx234@googlemail.com>
2026-01-29 23:59:04 +0100

    Use config for host addres

--- src/server_infra/start_app_store_server.rs
+++ src/server_infra/start_app_store_server.rs
@@ -1,12 +1,12 @@
 use std::net::SocketAddr;
-use store::server_infra::{app_init::app_init, app_state::ServerElements};
+use store::{config::Config, server_infra::{app_init::app_init, app_state::ServerElements}};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
+    let config = Config::new();
     let server_state = ServerElements::new();
     let router = app_init(server_state);
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
+    let listener = tokio::net::TcpListener::bind(config.get_host_socket_addr()).await?;
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, router.into_make_service_with_connect_info::<SocketAddr>()).await?;
     Ok(())
